a25c084f835eb55cc58b6e903bbad3313cdb0784
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-05-28 15:16:46 +0530

    Learn about If Statements: how to setup an if clause.

--- basics/boolean.js
+++ basics/boolean.js
@@ -5,22 +5,27 @@
 // >= - greater than or equal
 // <= - less than or equal
 
-// let temp = 100;
-// let freezing = temp <= 100;
+let temp = 180;
 
-// console.log(freezing);
+if (temp <= 32) {
+    console.log("It is freezing outside!");
+}
+
+if (temp >= 110) {
+    console.log("Ii is way to hot outside!");
+}
 
 
 // Challenge
-    // Create an age variable and set it to your age
-    // Calculate is child - if they are 7 or under
-    // Calculate is senior - if they are 65 or older
-    // Print is child value
-    // Print is senior value
+    // If 7 or under print message about child pricing
+    // If 65 or older print message about senior discount
 
-let age = 65;
-let isChild = age <= 7;
-let isSenior = age >= 65;
+let age = 6;
 
-console.log(isChild);
-console.log(isSenior);
+if (age <= 7) {
+    console.log("You will get a child discount");
+}
+
+if (age >= 65) {
+    console.log("You will get a senior discount");
+}

e6c50acb8594911dc14fe7370c94b04da4923766
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-05-28 14:06:50 +0530

    Complete a little challenge.

---
+++ basics/temp-conversion.js
@@ -0,0 +1,12 @@
+let fahrenheit = 50;
+
+// calculate celsius and store in celsius variable
+// print that value
+let celsius = (fahrenheit - 32) * 5 / 9;
+console.log("Temperature in celsius: " + celsius);
+
+
+// calculate kelvin value and store in variable
+// print that value
+let kelvin = (fahrenheit + 459.67) * 5 / 9;
+console.log("Temperature in kelvin: " + kelvin);

3c687ea0cad36e21b6cd715e429918b164ec7d3c
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-06-02 15:08:48 +0530

    Learn and understand that indexOf() uses triple equality operator behind the scene to go through array, to find an item. indexOf() is best for array of strings but this method is not worked for array of objects. Instead we use findIndex(), it doesn't uses triple equality opearator behind the scene, instead we provide our own search criteria

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = ["Note 1", "Note 2", "Note 3"];
+const notes = [{}, {
+    title: "My next trip",
+    body: "I would like to go to Spain"
+}, {
+    title: "Habbits to work on",
+    body: "Exercise. Eating a bit better."
+}, {
+    title: "Office modification",
+    body: "Get a new seat"
+}];
 
 // console.log(notes.pop());
 // notes.push("My new note");
@@ -8,29 +17,18 @@ const notes = ["Note 1", "Note 2", "Note 3"];
 
 // notes.splice(1, 0, "This is the new second item");
 
-notes[2] = "this is now the new note 3";
+// notes[2] = "this is now the new note 3";
 
-notes.forEach(function (item, index) {
-    console.log(index);
-    console.log(item);
-});
+// notes.forEach(function (item, index) {
+//     console.log(index);
+//     console.log(item);
+// });
 
 console.log(notes.length);
 console.log(notes);
 
-// Counting... 1
-for(let count = 1; count <=3; count++) {
-    console.log("Counting..." + count);
-}
+const index = notes.findIndex(function(note, index) {
+    return note.title === "Habbits to work on";
+});
 
-for(let count = 2; count >= 0; count--) {
-    console.log(count);
-}
-
-for(count = 0; count <= notes.length - 1; count++) {
-    console.log(notes[count]);
-}
-
-for(count = notes.length - 1; count >= 0; count--) {
-    console.log(notes[count]);
-}
+console.log(index);

d5f2d2ada50f57c58c0ab851a1560b24f1af3746
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-06-02 09:36:41 +0530

    Learn and understand arrays, how to create arrays, how to access array items

---
+++ arrays/todo.js
@@ -0,0 +1,8 @@
+// Create an array with five todos
+// You have x todos
+// Print the first and second to last items -> Todo: walk the dog
+const todos = ["Finish arrays section", "Take a bath", "Eat lunch", "Visit Labela", "Exercise"];
+
+console.log(`You have ${todos.length} todos`);
+console.log(`Todo: ${todos[0]}`);
+console.log(`Todo: ${todos[todos.length - 2]}`);

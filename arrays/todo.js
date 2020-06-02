8d53db66d395695b77fb75827e6d9068d24feb9f
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-06-02 10:16:04 +0530

    Learn how to manipulate arrays with methods, and complete a little challenge

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,8 +1,9 @@
-// Create an array with five todos
-// You have x todos
-// Print the first and second to last items -> Todo: walk the dog
 const todos = ["Finish arrays section", "Take a bath", "Eat lunch", "Visit Labela", "Exercise"];
 
+todos.splice(2, 1);
+todos.push("Listen songs");
+todos.shift();
+
 console.log(`You have ${todos.length} todos`);
-console.log(`Todo: ${todos[0]}`);
-console.log(`Todo: ${todos[todos.length - 2]}`);
+console.log(todos);
+console.log(todos);

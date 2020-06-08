fc549855b7fae75fad6d8879a943d7488048f6f3
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-06-08 09:45:03 +0530

    Learn and understand about DOM, how to query documents/elements, how to remove elements, how to read element value and change element value

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,12 @@
-console.log("This is from a different file!");
+// Query and remove
+// const p = document.querySelector("p");
+// p.remove();
+
+// Query all and remove
+const ps = document.querySelectorAll("p");
+
+ps.forEach(function (paragraph) {
+    paragraph.textContent = "******";
+    // console.log(paragraph.textContent);
+    // paragraph.remove();
+});
